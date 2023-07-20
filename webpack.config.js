0495c5be84c85c85cc895e0efa7e914ad5f9f38b
fruitbox12 <dylanwong007@gmail.com>
2023-07-20 12:59:00 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,3 +8,19 @@ module.exports = {
         }), ...
     ]
 };
+ config.resolve = {
+      fallback: {
+        fs: false,
+        tls: false,
+        net: false,
+        path: false,
+        zlib: false,
+        http: false,
+        https: false,
+        stream: false,
+        crypto: false,
+        url: false,
+        os: false,
+        assert: false
+      }
+    }
